1bd3eccf0a3430562398853320a9c5b5f1205288
rbardinet <rbardine@enssat.fr>
2021-11-16 17:37:39 +0100

    feat (Etape 1) : summarisation & normalisation

--- Src/rewriterFromCSV.py
+++ Src/rewriterFromCSV.py
@@ -21,15 +21,37 @@ class RewriterFromCSV(object):
 		f : Flight
 		try:
 			with open(self.dataFile, 'r') as source:
+				rewrite_global = None
+				count = 0
 				for line in source:
 					line = line.strip()
 					if line != "" and line[0] != "#":
 						f = Flight(line,self.vocabulary)
 						##Do what you need with the rewriting vector here ...
+
+						rewrite = f.rewrite()
 						print("-----------------------------")
 						print(f)
-						print("Rewritten flight :",f.rewrite())
+						print("Rewritten flight :", f.rewrite())
 						print("-----------------------------")
+
+						if rewrite_global == None :
+							rewrite_global = rewrite
+
+						else:
+							for key in rewrite:
+								rewrite_global[key] += rewrite[key]
+
+						count+=1
+				
+				for key in rewrite_global:
+					rewrite_global[key] = rewrite_global[key]/(count-1)
+					
+				print("Global flight atttribute sum : ", rewrite_global)
+
+
+
+
 
 		except:
 			raise Exception("Error while loading the dataFile %s"%(self.dataFile))
